74f17cea905896eb79304a39d8809b6034d9aee9
norberto <norberbtc@gmail.com>
2024-01-21 15:31:53 -0400

    Se agrego PI_APIs.py

---
+++ PI_APIs.py
@@ -0,0 +1,17 @@
+import requests
+def descargar_datos_csv(url):
+    # Realiza un GET request a la URL
+    response = requests.get(url)
+
+    # Verifica el código de estado de la respuesta
+    if response.status_code != 200:
+        raise Exception(f"Error al descargar los datos: {response.status_code}")
+
+    # Abre un archivo de texto plano con la extensión CSV
+    with open("datos.csv", "w") as archivo:
+
+        # Escribe la respuesta en el archivo
+        archivo.write(response.text)
+
+# Ejemplo de uso
+descargar_datos_csv("https://huggingface.co/datasets/mstz/heart_failure/raw/main/heart_failure_clinical_records_dataset.csv")
